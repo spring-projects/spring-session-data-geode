9024da840f3bc8181f4df1b1dfb6070de4e0dc9e
John Blum <jblum@pivotal.io>
2018-10-22 20:55:11 -0700

    Add Integration Tests to assert the configuration of custom session expiration policies.
    Resolve gh-5.

---
+++ spring-session-data-geode/src/integration-test/java/org/springframework/session/data/gemfire/expiration/CustomSessionExpirationConfigurationIntegrationTests.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.session.data.gemfire.expiration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Method;
+import java.time.Duration;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.RegionAttributes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.gemfire.config.annotation.PeerCacheApplication;
+import org.springframework.data.gemfire.tests.mock.annotation.EnableGemFireMockObjects;
+import org.springframework.session.Session;
+import org.springframework.session.data.gemfire.config.annotation.web.http.EnableGemFireHttpSession;
+import org.springframework.session.data.gemfire.config.annotation.web.http.GemFireHttpSessionConfiguration;
+import org.springframework.session.data.gemfire.expiration.config.SessionExpirationTimeoutAware;
+import org.springframework.session.data.gemfire.expiration.support.SessionExpirationPolicyCustomExpiryAdapter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.ReflectionUtils;
+
+/**
+ * Integration tests for {@link EnableGemFireHttpSession} and {@link GemFireHttpSessionConfiguration}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.apache.geode.cache.Region
+ * @see org.apache.geode.cache.RegionAttributes
+ * @see org.springframework.context.annotation.Bean
+ * @see org.springframework.data.gemfire.config.annotation.PeerCacheApplication
+ * @see org.springframework.data.gemfire.tests.mock.annotation.EnableGemFireMockObjects
+ * @see org.springframework.session.data.gemfire.config.annotation.web.http.EnableGemFireHttpSession
+ * @see org.springframework.session.data.gemfire.config.annotation.web.http.GemFireHttpSessionConfiguration
+ * @see org.springframework.session.data.gemfire.expiration.config.SessionExpirationTimeoutAware
+ * @see org.springframework.session.data.gemfire.expiration.support.SessionExpirationPolicyCustomExpiryAdapter
+ * @see org.springframework.test.context.junit4.SpringRunner
+ * @since 2.1.0
+ */
+@RunWith(SpringRunner.class)
+@SuppressWarnings("unused")
+public class CustomSessionExpirationConfigurationIntegrationTests {
+
+	@Resource(name = GemFireHttpSessionConfiguration.DEFAULT_SESSION_REGION_NAME)
+	private Region<String, Object> sessions;
+
+	@Autowired
+	private SessionExpirationPolicy sessionExpirationPolicy;
+
+	@SuppressWarnings("unchecked")
+	private <T> T invokeMethod(Object target, String methodName) throws NoSuchMethodException {
+
+		Method method = target.getClass().getDeclaredMethod(methodName);
+
+		ReflectionUtils.makeAccessible(method);
+
+		return (T) ReflectionUtils.invokeMethod(method, target);
+	}
+
+	@Test
+	public void sessionExpirationPolicyConfigurationIsCorrect() {
+
+		assertThat(this.sessionExpirationPolicy).isInstanceOf(TestSessionExpirationPolicy.class);
+		assertThat(this.sessionExpirationPolicy.expireAfter(null)).isEqualTo(Duration.ofMinutes(10));
+	}
+
+	@Test
+	public void regionIdleTimeoutExpirationConfigurationIsCorrect() throws Exception {
+
+		assertThat(this.sessions).isNotNull();
+
+		RegionAttributes<String, Object> sessionRegionAttributes = this.sessions.getAttributes();
+
+		assertThat(sessionRegionAttributes).isNotNull();
+		assertThat(sessionRegionAttributes.getCustomEntryIdleTimeout())
+			.isInstanceOf(SessionExpirationPolicyCustomExpiryAdapter.class);
+
+		SessionExpirationPolicyCustomExpiryAdapter adapter =
+			(SessionExpirationPolicyCustomExpiryAdapter) sessionRegionAttributes.getCustomEntryIdleTimeout();
+
+		assertThat(this.<SessionExpirationPolicy>invokeMethod(adapter, "getSessionExpirationPolicy"))
+			.isEqualTo(this.sessionExpirationPolicy);
+	}
+
+	@PeerCacheApplication
+	@EnableGemFireMockObjects
+	@EnableGemFireHttpSession(
+		maxInactiveIntervalInSeconds = 600,
+		sessionExpirationPolicyBeanName = "testSessionExpirationPolicy"
+	)
+	static class TestConfiguration {
+
+		@Bean
+		SessionExpirationPolicy testSessionExpirationPolicy() {
+			return new TestSessionExpirationPolicy();
+		}
+	}
+
+	static class TestSessionExpirationPolicy implements SessionExpirationPolicy, SessionExpirationTimeoutAware {
+
+		private Duration expirationTimeout;
+
+		@Override
+		public void setExpirationTimeout(Duration expirationTimeout) {
+			this.expirationTimeout = expirationTimeout;
+		}
+
+		@Override
+		public Duration expireAfter(Session session) {
+			return this.expirationTimeout;
+		}
+	}
+}
