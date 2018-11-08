dc12a8ebdeae7828ff4bcb276469f1d3dc918d62
John Blum <jblum@pivotal.io>
2018-11-07 23:57:34 -0800

    Edit Javadoc.

--- spring-session-data-geode/src/main/java/org/springframework/session/data/gemfire/config/annotation/web/http/support/SpringSessionGemFireConfigurer.java
+++ spring-session-data-geode/src/main/java/org/springframework/session/data/gemfire/config/annotation/web/http/support/SpringSessionGemFireConfigurer.java
@@ -92,6 +92,7 @@ public interface SpringSessionGemFireConfigurer {
 	 * @return the name of the {@link Pool} used by the {@link ClientCache} {@link Region}
 	 * to send {@link Session} state to the cluster of servers.
 	 * @see org.springframework.session.data.gemfire.config.annotation.web.http.GemFireHttpSessionConfiguration#DEFAULT_POOL_NAME
+	 * @see org.apache.geode.cache.client.Pool#getName()
 	 */
 	default String getPoolName() {
 		return GemFireHttpSessionConfiguration.DEFAULT_POOL_NAME;
@@ -105,6 +106,7 @@ public interface SpringSessionGemFireConfigurer {
 	 * @return a {@link String} specifying the name of the (client)cache {@link Region}
 	 * used to store {@link Session} state.
 	 * @see org.springframework.session.data.gemfire.config.annotation.web.http.GemFireHttpSessionConfiguration#DEFAULT_SESSION_REGION_NAME
+	 * @see org.apache.geode.cache.Region#getName()
 	 */
 	default String getRegionName() {
 		return GemFireHttpSessionConfiguration.DEFAULT_SESSION_REGION_NAME;
