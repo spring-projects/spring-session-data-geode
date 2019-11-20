02c9153f0298e87874db35b41c07b3006a4ad2fc
John Blum <jblum@pivotal.io>
2019-11-20 13:49:30 -0800

    Remove unnecessary 'throws Exception' clauses.
    Make GemfireOperationsAccesor a static inner abstract class.
    
    Format source code.

--- spring-session-data-geode/src/test/java/org/springframework/session/data/gemfire/GemFireOperationsSessionRepositoryTests.java
+++ spring-session-data-geode/src/test/java/org/springframework/session/data/gemfire/GemFireOperationsSessionRepositoryTests.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.springframework.session.data.gemfire;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -106,7 +105,7 @@ public class GemFireOperationsSessionRepositoryTests {
 
 	@Before
 	@SuppressWarnings("unchecked")
-	public void setup() throws Exception {
+	public void setup() {
 
 		AttributesMutator<Object, Session> mockAttributesMutator = mock(AttributesMutator.class);
 
@@ -356,7 +355,7 @@ public class GemFireOperationsSessionRepositoryTests {
 
 	@Test
 	@SuppressWarnings("unchecked")
-	public void findByPrincipalNameReturnsMatchingSessions() throws Exception {
+	public void findByPrincipalNameReturnsMatchingSessions() {
 
 		Session mockSessionOne = mock(Session.class, "MockSessionOne");
 		Session mockSessionTwo = mock(Session.class, "MockSessionTwo");
@@ -638,6 +637,6 @@ public class GemFireOperationsSessionRepositoryTests {
 			.publishEvent(isA(SessionDeletedEvent.class));
 	}
 
-	protected abstract class GemfireOperationsAccessor extends GemfireAccessor implements GemfireOperations { }
+	protected static abstract class GemfireOperationsAccessor extends GemfireAccessor implements GemfireOperations { }
 
 }
