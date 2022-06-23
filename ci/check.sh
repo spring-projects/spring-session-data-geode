e7784f8d33f8897ada90ad092fa081bfb583c9a1
John Blum <jblum@vmware.com>
2022-06-23 12:46:09 -0700

    Add Gradle clean task as a separate, independent build step for SSDG Jenkins CI Gradle builds.

--- ci/check.sh
+++ ci/check.sh
@@ -2,5 +2,7 @@
 
 set -eou pipefail
 
+./gradlew --no-daemon clean
+
 GRADLE_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home -Djava.io.tmpdir=/tmp" \
  ./gradlew check --no-daemon --refresh-dependencies --stacktrace
